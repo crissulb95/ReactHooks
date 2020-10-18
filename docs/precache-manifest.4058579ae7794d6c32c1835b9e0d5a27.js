7c0d2e51c2d447bd23c705ff5251574565cb71f5
Cristian <cristian.sulbaran.r@gmail.com>
2020-10-18 13:09:02 -0300

    docs agregado

---
+++ docs/precache-manifest.4058579ae7794d6c32c1835b9e0d5a27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd10a2e912d7155239b22a132b58dab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "897047c75f0f5a80c2e7",
+    "url": "/static/css/main.0ffaa161.chunk.css"
+  },
+  {
+    "revision": "c1e0b4d66b10a2e6546f",
+    "url": "/static/js/2.48fdec43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.48fdec43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897047c75f0f5a80c2e7",
+    "url": "/static/js/main.34e8a9aa.chunk.js"
+  },
+  {
+    "revision": "553b91d7ed2843beec38",
+    "url": "/static/js/runtime-main.78888243.js"
+  }
+]);
